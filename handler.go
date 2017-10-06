f66265bc3a8654e7185091c22cec50f8a6cebabe
otofu-square <yukita@feedforce.jp>
2017-10-06 21:52:34 +0900

    :recycle: Refactor by using serverless-golang's apigateway model

--- handler.go
+++ handler.go
@@ -3,21 +3,10 @@ package main
 import (
 	"github.com/eawsy/aws-lambda-go-core/service/lambda/runtime"
 	"github.com/eawsy/aws-lambda-go-event/service/lambda/runtime/event/apigatewayproxyevt"
+	"github.com/yunspace/serverless-golang/aws/event/apigateway"
 )
 
-type APIGatewayResponse struct {
-	StatusCode    int               `json:"statusCode"`
-	Headers       map[string]string `json:"headers"`
-	Body          interface{}       `json:"body"`
-	Base64Encoded bool              `json:"isBase64Encoded"`
-}
-
 func Ping(evt *apigatewayproxyevt.Event, ctx *runtime.Context) (interface{}, error) {
-	response := APIGatewayResponse{
-		StatusCode:    200,
-		Headers:       nil,
-		Body:          "pong",
-		Base64Encoded: false,
-	}
+	response := apigateway.NewAPIGatewayResponseWithBody(200, "Pong")
 	return response, nil
 }
