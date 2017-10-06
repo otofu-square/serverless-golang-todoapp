3a79ee1a9895c1a5ee957cbe4b39dd9bc969a5cb
otofu-square <yukita@feedforce.jp>
2017-10-06 20:53:26 +0900

    :tada: Add Ping handler

--- handler.go
+++ handler.go
@@ -5,7 +5,19 @@ import (
 	"github.com/eawsy/aws-lambda-go-event/service/lambda/runtime/event/apigatewayproxyevt"
 )
 
-/// Create
+type APIGatewayResponse struct {
+	StatusCode    int               `json:"statusCode"`
+	Headers       map[string]string `json:"headers"`
+	Body          interface{}       `json:"body"`
+	Base64Encoded bool              `json:"isBase64Encoded"`
+}
+
 func Ping(evt *apigatewayproxyevt.Event, ctx *runtime.Context) (interface{}, error) {
-	return "Pong", nil
+	response := APIGatewayResponse{
+		StatusCode:    200,
+		Headers:       nil,
+		Body:          "pong",
+		Base64Encoded: false,
+	}
+	return response, nil
 }
